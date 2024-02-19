c4ab1915e9f08889f5a39cf50c3d05f70b1750d2
D4rkisek <maruf.f@hotmail.com>
2024-02-19 22:31:26 +0000

    write down a thought process

---
+++ pystates_raphtory.py
@@ -0,0 +1,13 @@
+"""
+For Raphtory objects
+
+"""
+
+import Raphtory
+from pystates import all_spectrums, snapshot_dist
+
+
+# 1. Convert undirected edges into adjency matrices. Perhaps for each timestamp unit or perhaps give it a time window.
+#   if time window - for how long? - Could do an hour snapshots
+#
+# 2. ...
